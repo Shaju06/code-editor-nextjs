8cca5165e0d00d1f73f67de1202da4e1a2693ac3
= <=>
2024-04-21 13:48:37 +0530

    Added iframe to show code

---
+++ src/components/display-code.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function DisplayCode() {
+  return <h2>Hello WOrld</h2>;
+}
---
+++ src/plugin/unpkg-bundle.ts
@@ -0,0 +1,73 @@
+import axios from "axios";
+import * as esbuild from "esbuild-wasm";
+import localforage from "localforage";
+
+const cachedModule = localforage.createInstance({
+  name: "cachedModule",
+});
+
+export const unpkgPathPlugin = (inputVal: string) => {
+  return {
+    name: "unpkg-path-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+        };
+      });
+
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: inputVal,
+          };
+        } else {
+          const cachedRes = await cachedModule.getItem<esbuild.OnLoadResult>(
+            args.path
+          );
+
+          if (cachedRes) {
+            return cachedRes;
+          }
+
+          const { data, request } = await axios.get(args.path);
+
+          const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+
+          const contents =
+            fileType === "css"
+              ? `
+    const style = document.createElement('style');
+    style.innerText = '${data}';
+    document.head.appendChild(style)
+    
+    `
+              : data;
+
+          console.log(contents);
+
+          const result: esbuild.OnLoadResult = {
+            loader: "jsx",
+            contents,
+            resolveDir: new URL("./", request.responseURL).pathname,
+          };
+
+          await cachedModule.setItem(args.path, result);
+          return result;
+        }
+      });
+    },
+  };
+};
